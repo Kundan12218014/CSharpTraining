fdd3ff79f8014d3a906b6a96d662b49d5d4514f8
Kundan Kumar <kundankumar08950@gmail.com>
2026-02-24 14:53:22 +0530

    Javascript datatypes

---
+++ DAY(24-02-2026)/LearingJavascript/JavascriptDataTypes.js
@@ -0,0 +1,14 @@
+var name = "Kundan Kumar";
+var age = 30;
+var isStudent = true;
+var hobbies = ['reading', 'travelling', 'coding'];
+var address = {
+  street: "123 Main street",
+  city: "Antime",
+  country: "UDS"
+};
+console.log("Name: " + name);
+console.log("Age : " + age);
+console.log("Is studentL " + isStudent);
+console.log("Hobbies : " + hobbies.join(", "));
+console.log("Address : " + address.street + ", " + address.city + ", " + address.country);
