d6c81d24c5a17e8181c1db4117c3291fe363a793
Kundan Kumar <kundankumar08950@gmail.com>
2026-03-12 11:59:50 +0530

    version 1 Containing

---
+++ DAY(09-03-2026)/StudentManagementSystem/StudentManagementSystem/Controllers/StudentController.cs
@@ -0,0 +1,106 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using StudentManagementSystem.Models;
+using StudentManagementSystem.Models.Context;
+
+namespace StudentManagementSystem.Controllers
+{
+    public class StudentController : Controller
+    {
+        private StudentDbContext _context;
+        public StudentController(StudentDbContext studentDbContext)
+        {
+           this._context = studentDbContext;
+        }
+        public IActionResult Index()
+        {
+            var students = _context.Students
+                            .Include(s => s.Department)
+                            .ToList();
+
+            return View(students);
+        }
+        public IActionResult Create()
+        {
+            ViewBag.Departments = _context.Departments.ToList();
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Student student)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Students.Add(student);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            // The collection must be re-populated if validation fails
+            ViewBag.Departments = _context.Departments.ToList();
+            return View(student);
+        }
+
+        public IActionResult Edit(int id)
+        {
+            var student = _context.Students.Find(id);
+            if (student == null)
+            {
+                return NotFound();
+            }
+            ViewBag.Departments = _context.Departments.ToList();
+            return View(student);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Student student)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Students.Update(student);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.Departments = _context.Departments.ToList();
+            return View(student);
+        }
+
+        public IActionResult Details(int id)
+        {
+            var student = _context.Students
+                            .Include(s => s.Department)
+                            .FirstOrDefault(s => s.StudentId == id);
+
+            if (student == null)
+            {
+                return NotFound();
+            }
+
+            return View(student);
+        }
+
+        public IActionResult Delete(int id)
+        {
+            var student = _context.Students
+                            .Include(s => s.Department)
+                            .FirstOrDefault(s => s.StudentId == id);
+
+            if (student == null)
+            {
+                return NotFound();
+            }
+
+            return View(student);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeleteConfirmed(int id)
+        {
+            var student = _context.Students.Find(id);
+            if (student != null)
+            {
+                _context.Students.Remove(student);
+                _context.SaveChanges();
+            }
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ DAY(09-03-2026)/StudentManagementSystem/StudentManagementSystem/Models/Student.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.EntityFrameworkCore;
+namespace StudentManagementSystem.Models
+
+{
+    public class Student
+    {
+        public int StudentId { get; set; }
+        [Required]
+       
+        public string Name { get; set; }
+
+        [Range(18,60)]
+        public int Age { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        public string? Password { get; set; }
+        [EmailAddress]
+        public string Email { get; set; }
+        public int DepartmentId { get; set; }
+        public string? Gender { get; set; }
+        public Department? Department { get; set; }
+    }
+}
